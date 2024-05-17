8d55b792a4107fb61f7f9e9958deecf62fcf6efc
Cristian Blanco <ccamiloblanco@gmail.com>
2024-05-17 00:09:10 -0500

    Subida y configuración de fuentes

--- assets/js/particles.js
+++ assets/js/particles.js
@@ -1,6 +1,6 @@
 particlesJS("particles-js", {
   particles: {
-    number: { value: 280, density: { enable: true, value_area: 800 } },
+    number: { value: 100, density: { enable: true, value_area: 800 } },
     color: { value: "#ffffff" },
     shape: {
       type: "circle",
@@ -9,20 +9,20 @@ particlesJS("particles-js", {
       image: { src: "img/github.svg", width: 100, height: 100 },
     },
     opacity: {
-      value: 0.3,
+      value: 0.2,
       random: false,
       anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false },
     },
     size: {
-      value: 4,
+      value: 3,
       random: true,
       anim: { enable: false, speed: 40, size_min: 0.1, sync: false },
     },
     line_linked: {
-      enable: false,
+      enable: true,
       distance: 150,
       color: "#ffffff",
-      opacity: 0.4,
+      opacity: 0.2,
       width: 1,
     },
     move: {
@@ -46,7 +46,7 @@ particlesJS("particles-js", {
     modes: {
       grab: { distance: 400, line_linked: { opacity: 1 } },
       bubble: { distance: 400, size: 40, duration: 2, opacity: 8, speed: 3 },
-      repulse: { distance: 100, duration: 1.4 },
+      repulse: { distance: 150, duration: 1.4 },
       push: { particles_nb: 4 },
       remove: { particles_nb: 2 },
     },
